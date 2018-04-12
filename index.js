6590ae1150ea4fbb5a21314b5f1040a13bec05e0
Prathyusha Mattam <prathyusha.mattam@gmail.com>
2018-04-11 21:18:21 -0400

    Added author to blogsList

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const root = document.querySelector('.react-root');
 const h = React.createElement;
 
 let blogs = [
-    { title: 'title1', body: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.' },
-    { title: 'title2', body: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.' }
+    { title: 'title1', author: 'author1', body: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.' },
+    { title: 'title2', author: 'author2', body: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.' },
+    { title: 'title3', author: 'author3', body: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable.' },
+    { title: 'title4', author: 'author4', body: 'If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text.' },
 ];
 
 let BlogList = ({ blogs }) => {
@@ -11,6 +13,7 @@ let BlogList = ({ blogs }) => {
     for (let blog of blogs) {
         let blogVdom = h('div', null, [
             h('h1', null, blog.title),
+            h('h2', null, blog.author),
             h('p', null, blog.body),
         ])
         vdoms.push(blogVdom);
