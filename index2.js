47fae8287934f636a50dced232714c6be167815c
Prathyusha Mattam <prathyusha.mattam@gmail.com>
2018-04-11 21:05:00 -0400

    adding component and wrapping them

--- index2.js
+++ index2.js
@@ -1,7 +1,11 @@
 const root = document.querySelector('.react-root');
 const h = React.createElement;
 
-let h1 = h('h1', { className: 'header' }, 'Hello Prathyusha!');
+let generateGreeting = (name) => {
+    return h('h1', { className: 'header' }, `Hello ${name}`);
+}
+
+let h1 = generateGreeting('Prathyusha');
 let p = h('p', {}, 'Here is some text!');
 let footer = h('footer', null, 'copyright 2018');
 
