5a7a252dc39dcc1c3dc0dbd2726bfab18c7a0b60
Prathyusha Mattam <prathyusha.mattam@gmail.com>
2018-04-13 12:34:29 -0400

    Deleting a blog changes the state and so moved it into the Page component

--- index.js
+++ index.js
@@ -2,20 +2,13 @@ const root = document.querySelector('.react-root');
 const h = React.createElement;
 
 let blogs = [
-    { id: '1', title: 'Veggie Ipsum 1', author: 'Author 1', body: 'Veggies es bonus vobis' }
-    // { id: '2', title: 'Veggie Ipsum 2', author: 'Author 2', body: 'Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale.' },
-    // { id: '3', title: 'Veggie Ipsum 3', author: 'Author 3', body: 'Grape wattle seed kombu beetroot horseradish carrot squash brussels sprout chard.' },
-    // { id: '4', title: 'Veggie Ipsum 4', author: 'Author 4', body: 'Beetroot water spinach okra water chestnut ricebean pea catsear courgette summer purslane.' },
+    { id: '1', title: 'Veggie Ipsum 1', author: 'Author 1', body: 'Veggies es bonus vobis' },
+    { id: '2', title: 'Veggie Ipsum 2', author: 'Author 2', body: 'Turnip greens ricebean' },
+    { id: '3', title: 'Veggie Ipsum 3', author: 'Author 3', body: 'Grape wattle seed' },
+    { id: '4', title: 'Veggie Ipsum 4', author: 'Author 4', body: 'Beetroot water spinach okra' },
 ];
 
 let currentlyEditingBlog = null;
-
-let removeBlog = (blog) => {
-    let { id } = blog;
-    blogs = blogs.filter(blog => id !== blog.id);
-    console.log(`Removed ${blog.title}`);
-    updatePage();
-};
 
 let changeEditBlogState = (blogState) => {
     currentlyEditingBlog = Object.assign({}, blogState);
@@ -35,7 +28,7 @@ let saveUpdatedBlog = (blogBeingEditedToSave) => {
     updatePage();
 };
 
-let DeleteButton = (blog) => h('button', { onClick: () => removeBlog(blog) }, 'Delete');
+let DeleteButton = ({ blog, removeBlog }) => h('button', { onClick: () => removeBlog(blog) }, 'Delete');
 
 let EditButton = (blogToEdit) => h('button', { onClick: () => changeEditBlogState(blogToEdit) }, 'Edit');
 
@@ -44,17 +37,17 @@ let EditBlogForm = (blogToEditInForm) => h('form', null, [
     h('button', { onClick: () => saveUpdatedBlog(currentlyEditingBlog) }, 'Save')
 ]);
 
-let BlogRow = (blog) =>
+let BlogRow = ({ blog, removeBlog }) =>
     h('ul', null, [
         h('h2', null, blog.title),
         h('h3', null, blog.author),
         h('p', null, blog.body),
-        h(DeleteButton, blog),
+        h(DeleteButton, { blog, removeBlog }),
         h(EditButton, blog),
         currentlyEditingBlog && blog.id === currentlyEditingBlog.id && h(EditBlogForm, blog)
     ]);
 
-let BlogList = ({ blogs }) => h('div', null, blogs.map(blog => h(BlogRow, blog, [])));
+let BlogList = ({ blogs, removeBlog }) => h('div', null, blogs.map(blog => h(BlogRow, { blog, removeBlog })));
 
 let Title = () => h('h1', null, 'About Veggies Ipsum Blog');
 
@@ -62,21 +55,11 @@ let Greeting = ({ person }) => h('h1', { className: 'header' }, `Good Morning ${
 
 let Footer = () => h('footer', null, 'copyright 2018');
 
-// let Page = () => h('div', null, [
-//     h(Title, null, []),
-//     h(Greeting, { person: 'Prathyusha' }, []),
-//     h(BlogList, { blogs: blogs }, []),
-//     h(Footer, null, [])
-// ]);
-
-// let updatePage = () => ReactDOM.render(h(Page, { blogs: blogs }, []), root);
-
-// updatePage();
-
 class Page extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            /* this stateBlogs has value of blogs array at the top */
             blogs: blogs,
             currentlyEditingBlog: null
         }
@@ -85,11 +68,23 @@ class Page extends React.Component {
     render() {
         let { blogs, currentlyEditingBlog } = this.state;
 
+        /* state changes here when we remove a blog so moving it in here so when it does Page component handles rendering */
+        let removeBlog = (rmBlog) => {
+            let { id } = rmBlog;
+            // console.log("THIS STATE BLOGS", this.state.stateBlogs);
+            // let removedBlog = this.state.stateBlogs.filter(blog => id !== blog.id);
+            let updatedBlogs = blogs.filter(blog => id !== blog.id);
+            /* setState and update the data source */
+            this.setState({
+                blogs: updatedBlogs
+            });
+        };
+
         return h('div', null, [
-            h(Title, null, []),
-            h(Greeting, { person: 'Prathyusha' }, []),
-            h(BlogList, { blogs: blogs }, []),
-            h(Footer, null, [])
+            h(Title),
+            h(Greeting, { person: 'Prathyusha' }),
+            h(BlogList, { blogs, removeBlog }),
+            h(Footer)
         ]);
     }
 }
