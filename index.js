344fc8507d5fa5259d4a9dad07fcb0585e3de6de
Prathyusha Mattam <prathyusha.mattam@gmail.com>
2018-04-12 21:03:39 -0400

    Adding Save button to save edited blog

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const root = document.querySelector('.react-root');
 const h = React.createElement;
 
 let blogs = [
-    { id: '1', title: 'Veggie Ipsum 1', author: 'Author 1', body: 'Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.', isEditable: false },
-    { id: '2', title: 'Veggie Ipsum 2', author: 'Author 2', body: 'Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale.', isEditable: false },
-    { id: '3', title: 'Veggie Ipsum 3', author: 'Author 3', body: 'Grape wattle seed kombu beetroot horseradish carrot squash brussels sprout chard.', isEditable: false },
-    { id: '4', title: 'Veggie Ipsum 4', author: 'Author 4', body: 'Beetroot water spinach okra water chestnut ricebean pea catsear courgette summer purslane.', isEditable: false },
+    { id: '1', title: 'Veggie Ipsum 1', author: 'Author 1', body: 'Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.' },
+    { id: '2', title: 'Veggie Ipsum 2', author: 'Author 2', body: 'Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale.' },
+    { id: '3', title: 'Veggie Ipsum 3', author: 'Author 3', body: 'Grape wattle seed kombu beetroot horseradish carrot squash brussels sprout chard.' },
+    { id: '4', title: 'Veggie Ipsum 4', author: 'Author 4', body: 'Beetroot water spinach okra water chestnut ricebean pea catsear courgette summer purslane.' },
 ];
+
+let currentlyEditingBlog = null;
 
 let removeBlog = (blog) => {
     let { id } = blog;
@@ -16,16 +18,20 @@ let removeBlog = (blog) => {
 };
 
 let changeEditBlogState = (blogState) => {
-    let blog = blogs.find(blog => blog.id === blogState.id);
-    blog.isEditable = !blog.isEditable;
-    console.log(blog.isEditable);
+    currentlyEditingBlog = Object.assign({}, blogState);
     updatePage();
 };
 
 let updateBlogBody = (blogObject, value) => {
     let blog = blogs.find(blog => blog.id === blogObject.id);
-    console.log(blog);
-    blog.body = value;
+    blogObject.body = value;
+    updatePage();
+};
+
+let saveUpdatedBlog = (blogBeingEditedToSave) => {
+    let blog = blogs.find(blog => blog.id === blogBeingEditedToSave.id);
+    Object.assign(blog, blogBeingEditedToSave);
+    currentlyEditingBlog = null;
     updatePage();
 };
 
@@ -33,7 +39,10 @@ let DeleteButton = (blog) => h('button', { onClick: () => removeBlog(blog) }, 'D
 
 let EditButton = (blogToEdit) => h('button', { onClick: () => changeEditBlogState(blogToEdit) }, 'Edit');
 
-let EditBlogForm = (blogToEditInForm) => h('form', null, [h('input', { 'value': blogToEditInForm.body, onChange: (event) => updateBlogBody(blogToEditInForm, event.target.value) })]);
+let EditBlogForm = (blogToEditInForm) => h('form', null, [
+    h('input', { 'value': currentlyEditingBlog.body, onChange: (event) => updateBlogBody(currentlyEditingBlog, event.target.value) }),
+    h('button', { onClick: () => saveUpdatedBlog(currentlyEditingBlog) }, 'Save')
+]);
 
 let BlogRow = (blog) =>
     h('ul', null, [
@@ -42,7 +51,7 @@ let BlogRow = (blog) =>
         h('p', null, blog.body),
         h(DeleteButton, blog),
         h(EditButton, blog),
-        blog.isEditable && h(EditBlogForm, blog)
+        currentlyEditingBlog && blog.id === currentlyEditingBlog.id && h(EditBlogForm, blog)
     ]);
 
 let BlogList = ({ blogs }) => h('div', null, blogs.map(blog => h(BlogRow, blog, [])));
