7fb89b77d5cfa7080afbcffea55357a328c8cccf
Prathyusha Mattam <prathyusha.mattam@gmail.com>
2018-04-13 21:29:25 -0400

    Changed everything over to jsx

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const root = document.querySelector('.react-root');
-const h = React.createElement;
 
 let blogs = [
     { id: '1', title: 'Veggie Ipsum 1', author: 'Author 1', body: 'Veggies es bonus vobis' },
@@ -8,40 +7,74 @@ let blogs = [
     { id: '4', title: 'Veggie Ipsum 4', author: 'Author 4', body: 'Beetroot water spinach okra' },
 ];
 
-let DeleteButton = ({ blog, removeBlog }) => h('button', { onClick: () => removeBlog(blog) }, 'Delete');
+let DeleteButton = ({ blog, removeBlog }) => 
+    <button onClick={() => removeBlog(blog)}>Delete</button>
 
-let EditButton = ({ blog, changeEditBlogState }) => h('button', { onClick: () => changeEditBlogState(blog) }, 'Edit');
+let EditButton = ({ blog, changeEditBlogState }) => 
+    <button onClick={() => changeEditBlogState(blog)}>Edit</button>
 
-let EditBlogForm = ({ blog, currentlyEditingBlog, updateBlogTitle, updateBlogBody, saveUpdatedBlog }) => h('form', null, [
-    h('input', { 'value': currentlyEditingBlog.title, onChange: (event) => updateBlogTitle(currentlyEditingBlog, event.target.value) }),
-    h('input', { 'value': currentlyEditingBlog.body, onChange: (event) => updateBlogBody(currentlyEditingBlog, event.target.value) }),
-    h('button', { onClick: () => saveUpdatedBlog(currentlyEditingBlog) }, 'Save')
-]);
+let EditBlogForm = ({ blog, currentlyEditingBlog, updateBlogTitle, updateBlogBody, saveUpdatedBlog }) => 
+    <form>
+        <input key="1" value={currentlyEditingBlog.title} onChange={(event) => updateBlogTitle(currentlyEditingBlog, event.target.value)} />
+        <input key="2" value={currentlyEditingBlog.body} onChange={(event) => updateBlogBody(currentlyEditingBlog, event.target.value)} />
+        <button onClick={() => saveUpdatedBlog(currentlyEditingBlog)}>Save</button>
+    </form>
 
 let BlogRow = ({ blog, currentlyEditingBlog, removeBlog, changeEditBlogState, updateBlogTitle, updateBlogBody, saveUpdatedBlog }) =>
-    h('ul', null, [
-        h('h2', null, blog.title),
-        h('h3', null, blog.author),
-        h('p', null, blog.body),
-        h(DeleteButton, { blog, removeBlog }),
-        h(EditButton, { blog, changeEditBlogState }),
-        currentlyEditingBlog && blog.id === currentlyEditingBlog.id && h(EditBlogForm, { blog, currentlyEditingBlog, updateBlogTitle, updateBlogBody, saveUpdatedBlog })
-    ]);
+    <div>
+        <h2>{blog.title}</h2>
+        <h3>{blog.author}</h3>
+        <p>{blog.body}</p>
+        <DeleteButton 
+            blog={blog}
+            removeBlog={removeBlog}
+        />
+        <EditButton 
+            blog={blog}
+            changeEditBlogState={changeEditBlogState}
+        />
+        {
+            currentlyEditingBlog && blog.id === currentlyEditingBlog.id && 
+            <EditBlogForm 
+                blog={blog}
+                currentlyEditingBlog={currentlyEditingBlog}
+                updateBlogTitle={updateBlogTitle}
+                updateBlogBody={updateBlogBody}
+                saveUpdatedBlog={saveUpdatedBlog}
+            />
+        }
+    </div>
 
 let BlogList = ({ blogs, currentlyEditingBlog, removeBlog, changeEditBlogState, updateBlogTitle, updateBlogBody, saveUpdatedBlog }) =>
-    h('div', null, blogs.map(blog => h(BlogRow, { blog, currentlyEditingBlog, removeBlog, changeEditBlogState, updateBlogTitle, updateBlogBody, saveUpdatedBlog })));
+    <div>
+        {
+            blogs.map(blog =>
+            <BlogRow 
+                blog={blog}
+                currentlyEditingBlog={currentlyEditingBlog}
+                removeBlog={removeBlog}
+                changeEditBlogState={changeEditBlogState}
+                updateBlogTitle={updateBlogTitle}
+                updateBlogBody={updateBlogBody}
+                saveUpdatedBlog={saveUpdatedBlog}    
+            />)
+        }        
+    </div>
 
-let Title = () => h('h1', null, 'About Veggies Ipsum Blog');
+let Title = () => 
+    <h1>About Veggie Ipsum Blog</h1>
 
-let Greeting = ({ person }) => h('h1', { className: 'header' }, `Good Morning ${person} !`);
+let Greeting = ({ person }) => 
+    <h1 className="header">Good Morning {person}!</h1>
 
-let Footer = () => h('footer', null, 'copyright 2018');
+let Footer = () => 
+    <footer>copyright 2018</footer>
 
 class Page extends React.Component {
+
     constructor(props) {
         super(props);
         this.state = {
-            /* this blogs has value of blogs array at the top */
             blogs: blogs,
             currentlyEditingBlog: null
         }
@@ -49,11 +82,9 @@ class Page extends React.Component {
 
     render() {
         let { blogs, currentlyEditingBlog } = this.state;
-        /* state changes here when we remove a blog so moving it in here so when it does Page component handles rendering */
         let removeBlog = (rmBlog) => {
             let { id } = rmBlog;
             let updatedBlogs = blogs.filter(blog => id !== blog.id);
-            /* setState and update the data source */
             this.setState({
                 blogs: updatedBlogs
             });
@@ -85,7 +116,6 @@ class Page extends React.Component {
             this.setState({    
                 currentlyEditingBlog: null
             });
-
         };
                 
         return (
@@ -101,9 +131,9 @@ class Page extends React.Component {
                     updateBlogBody={updateBlogBody}
                     saveUpdatedBlog={saveUpdatedBlog}
                 />
+                <Footer />
             </div>
-        )
-            
+        )      
     }
 }
 
